7ecc96a80635d397370a80aa8c715fa14a89c420
LucasPereira <lucas970997@gmail.com>
2021-09-27 21:34:23 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "i2kdIbn0Uoc",
+        title: "Show do messi em vitória no Santiago Bernabéu",
+        duration: "1:43H",
+        price: "free"
+    },
+    {
+        id: "h4m68r8kWAc",
+        title: "Maior virada da história contra a equipe do PSG.",
+        duration: "08 MIN",
+        price: "free"
+    },
+    {
+        id: "O0tt0r5cv0M",
+        title: "Resultado inacreditável em cima do Milan.",
+        duration: "06 MIN",
+        price: "free"
+    },
+    {
+        id: "lWDb3XuycH8",
+        title: "A classificação polêmica contra o Chelsea.",
+        duration: "10 MIN",
+        price: "free"
+    },
+    {
+        id: "3_OPwntMz84",
+        title: "Título da champions conquistado em 2015",
+        duration: "08 MIN",
+        price: "free"
+    },
+    {
+        id: "ZPG1NR5BbnU",
+        title: "Ronaldinho sendo aplaudido de pé no bernabéu",
+        duration: "05 MIN",
+        price: "free"
+    }
+]
